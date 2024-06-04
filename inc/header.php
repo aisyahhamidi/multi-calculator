b42ce2e77bfe999ece37e29e77df0eff2d7e1ca2
shazwihaziq <163448457+shazwihaziq@users.noreply.github.com>
2024-06-05 00:59:55 +0800

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,6 @@
 <head>
 <?php require_once ('data/config.php')?>
-	<title><?php echo $site_name ;?></title>
+	
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" 	content="ie=edge" />
