c18a81b77864b9e1fc8acc2f33ad3015b5101f3e
shazwihaziq <163448457+shazwihaziq@users.noreply.github.com>
2024-06-05 00:45:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,7 @@
 <!DOCTYPE html>
 <html>
-
-
-
+<?php require_once ('inc/header.php')?>
 <head>
-<title>Button Menu</title>
-<style>
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    font-family: sans-serif;
-  }
-  .button-container {
-    display: flex;
-    gap: 10px;
-  }
-  .button {
-    padding: 10px 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-</style>
+<title>Calculator</title>
 </head>
-<body>
-  <div class="button-container">
-    <a href="indexloan.php" class="button">Loan</a>
-    <a href="indexbmi.php" class="button">BMI</a>
-    <a href="indexelectricity.php" class="button">Electricity</a>
-  </div>
-</body>
 </html>
