2f583db461c22f2af62da76e7a570df9b2f4b605
shazwihaziq <163448457+shazwihaziq@users.noreply.github.com>
2024-06-05 01:06:36 +0800

    Update config.php

--- data/config.php
+++ data/config.php
@@ -1,17 +1,10 @@
 <?php 
-/*
-||||================================================||||
-||||================================================||||
-||||        	LOAN CALCULATOR By Mohammed Cha     ||||
-||||================================================||||
-||||================================================||||
-*/
 
 $website_url = 'https://www.website.com';
 $master_email = 'contact@gmail.com';
 
-$site_name = 'Auto Loan Calculator';
-$site_descr = 'Calculate payments over the life of your Loan';
+$site_name = 'Multiple Calculators';
+$site_descr = 'Your One-Stop Calculator Center';
 $site_keywords = 'Calculate, payments, Loan, loans, bank';
 $currency = ' $ ';
 
